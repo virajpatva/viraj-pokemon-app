e0f5ac41dc463e44beb560eb3ede43d414bc707e
viraj.patva <viraj.patva@smartsensesolutions.com>
2023-04-04 15:13:55 +0530

    initial setup

---
+++ pokemon-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import LoginPage from './pages/1-login-page'
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  )
+}
+
+export default App
---
+++ pokemon-app/src/pages/1-login-page.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function LoginPage() {
+  return (
+    <>
+      <p>Login Page</p>
+    </>
+  )
+}
+
+export default LoginPage;
