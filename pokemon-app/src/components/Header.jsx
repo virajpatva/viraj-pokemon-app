519383c944e73b7acab267b45c603455a7c91819
viraj.patva <viraj.patva@smartsensesolutions.com>
2023-04-07 09:24:19 +0530

    feat : header css added

--- pokemon-app/src/components/Header.jsx
+++ pokemon-app/src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Header() {
   return (
-    <>
+    <div className='header'>
       <h1>Welcome to Pokemon Application {name}</h1>
-    </>
+    </div>
   )
 }
 
